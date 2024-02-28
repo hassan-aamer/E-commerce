ae7b91e3de2eae4532e623b13be1870e4c142468
hassan-aamer <hassanaamer048@gmail.com>
2024-02-28 23:24:36 +0200

    Update

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+
 dotenv.config({path: 'config.env'});
 
 const app = express();
 
+if(process.env.NODE_ENV == "development"){
+    app.use(morgan('dev'));
+    console.log(`mode: ${process.env.NODE_ENV}`);
+}
+
 app.get('/',(req, res) => {
     res.send('Welcome Node JS Api');
-})
+});
 
 const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => {
     console.log('App Running on Port 9000');
-})
+});
