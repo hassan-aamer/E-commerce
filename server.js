c14da394946b5741c76fe5506743990b431f458c
hassan-aamer <hassanaamer048@gmail.com>
2024-02-28 22:29:09 +0200

    Update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req, res) => {
-    res.send('Welcome Node JS ي');
+    res.send('Welcome Node JS');
 })
 app.listen(8000, () => {
     console.log('App Running...');
