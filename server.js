281bda453d653f7379cdf1b2437dc3ea503740c4
hassan-aamer <hassanaamer048@gmail.com>
2024-02-29 19:52:42 +0200

    Add MongoDB and Mongoose to the project

--- server.js
+++ server.js
@@ -1,21 +1,35 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const morgan = require('morgan');
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 
-dotenv.config({path: 'config.env'});
+// Connect With DB
+mongoose
+    .connect(process.env.DB_URI)
+    .then((conn) => {
+        console.log(`Database Connected: ${conn.connection.host}`);
+    })
+    .catch((err) => {
+        console.error(`Database Error: ${err}`);
+        process.exit(1);
+    });
+
+
+
+dotenv.config({ path: "config.env" });
 
 const app = express();
 
-if(process.env.NODE_ENV == "development"){
-    app.use(morgan('dev'));
+if (process.env.NODE_ENV == "development") {
+    app.use(morgan("dev"));
     console.log(`mode: ${process.env.NODE_ENV}`);
 }
 
-app.get('/',(req, res) => {
-    res.send('Welcome Node JS Api');
+app.get("/", (req, res) => {
+    res.send("Welcome Node JS Api");
 });
 
 const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => {
-    console.log('App Running on Port 9000');
+    console.log("App Running on Port 9000");
 });
